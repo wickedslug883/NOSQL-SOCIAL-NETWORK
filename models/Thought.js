0cdbf03028af27217389481dcf098606c8da260e
slug <amadrildanny@gmail.com>
2023-01-31 01:43:04 -0800

    Update Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -4,9 +4,7 @@ const dateFormat = require("../utils/dateFormat");
 const ReactionSchema = new Schema(
   {
     reactionId: {
-      // Mongoose's ObjectId data type
       type: Schema.Types.ObjectId,
-      // Default value is set to a new ObjectId
       default: () => new Types.ObjectId(),
     },
 
@@ -23,9 +21,7 @@ const ReactionSchema = new Schema(
 
     createdAt: {
       type: Date,
-      // Set default value to the current timestamp
       default: Date.now,
-      // Use a getter method to format the timestamp on query
       get: (timestamp) => dateFormat(timestamp),
     },
   },
@@ -49,7 +45,6 @@ const ThoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      // Use a getter method to format the timestamp on query
       get: (timestamp) => dateFormat(timestamp),
     },
 
@@ -57,8 +52,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true,
     },
-
-    // array of nested documents created with the reactionSchema
+    
     reactions: [ReactionSchema],
   },
   {
